da113f99d4f98ae43b38af70219aed91c13911e4
bzk9x <bzk9xx@gmail.com>
2025-09-07 23:25:17 +0100

    feat: Add initial Activities and navigation logic
    This commit introduces the basic Activity structure and initial navigation flow for the Tether app.
    
    Key changes:
    - Added `MainActivity`, `OnboardingActivity`, and `HomeActivity` with their corresponding layout files.
    - Implemented logic in `MainActivity` to check if it's the user's first time opening the app:
        - If it's the first time, navigate to `OnboardingActivity`.
        - Otherwise, navigate to `HomeActivity` after a short delay.
    - Configured `MainActivity` as the launcher activity in `AndroidManifest.xml`.
    - Added necessary dependencies for `Activity` and `ConstraintLayout`.
    - Updated the base application theme in `themes.xml` to `Theme.MaterialComponents.DayNight.NoActionBar` and set a default window background color.
    - Included IDE configuration files for App Insights and VCS mappings.
    - Added a placeholder image `daisy.png` to drawables.

---
+++ Android/app/src/main/java/com/bittscafe/tether/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.bittscafe.tether
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.edit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
+
+        val prefs = this.getSharedPreferences("prefs", MODE_PRIVATE)
+        val isFirstTime = prefs.getBoolean("is_first_time", true)
+
+        if (isFirstTime) {
+            prefs.edit { putBoolean("is_first_time", false) }
+            val onboardingActivity = Intent(this, OnboardingActivity::class.java)
+            startActivity(onboardingActivity)
+            finish()
+        } else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                val homeActivity = Intent(this, HomeActivity::class.java)
+                startActivity(homeActivity)
+                finish()
+            }, 2000)
+        }
+    }
+}
